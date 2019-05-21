0b7a7e00da4e7acc76c5ff8fa3e099d4221cfc22
rockergaik <39429508+rockergaik@users.noreply.github.com>
2019-05-21 17:17:27 +0700

    projek akhir
    projek akhir AJT

---
+++ bleserver.ino
@@ -0,0 +1,39 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+
+#define SERVICE_UUID        "85405da8-7ad3-11e9-8f9e-2a86e4085a59"
+#define CHARACTERISTIC_UUID "d135d5db-4890-4439-907c-4b02c019f5a4"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Starting BLE work!");
+
+  BLEDevice::init("MyESP32");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+                                         CHARACTERISTIC_UUID,
+                                         BLECharacteristic::PROPERTY_READ |
+                                         BLECharacteristic::PROPERTY_WRITE
+                                       );
+
+  pCharacteristic->setValue("oke");
+  pService->start();
+  BLEAdvertising *pAdvertising = pServer->getAdvertising();  // this still is working for backward compatibility
+  pAdvertising->start();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);  // functions that help with iPhone connections issue
+  pAdvertising->setMinPreferred(0x12);
+  BLEDevice::startAdvertising();
+  Serial.println("Characteristic defined! Now you can read it in your phone!");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(2000);
+}
